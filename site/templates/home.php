742188a73a5c27ebc70e58570dfd21e21115d1f3
Jeff Ong <jonger4@gmail.com>
2017-08-06 23:58:37 -0400

    init brutal

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,15 +1,14 @@
+<?php
+
+$projects = page('projects')->children()->visible();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Jeff Ong | Designer Technologist Artist</title>
     <link href="https://fonts.googleapis.com/css?family=Rasa|Roboto" rel="stylesheet">
-    <style>
-        body {
-            width: 35em;
-            margin: 0 auto;
-            font-family: 'Rasa', Tahoma, Verdana, Arial, sans-serif;
-        }
-    </style>
+    <link href="/assets/css/index.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="description" content="Portfolio media for Jeff Ong">
     <meta name="keywords" content="design,technology,code,media,itp,frog,art,jeff ong,jeffrey ong">
@@ -17,14 +16,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-    <h1>Jeff Ong</h1>
-    <h2><marquee>Welcome to my personal website!</marquee></h2>
-    <p>I'm an American artist, designer, technologist based in Brooklyn, New York.</p>
+    <div class="container">
 
-    <a href="mailto:this.jeffong@gmail.com">Email</a>.<br/>
-    <a href="https://github.com/jffng">GitHub</a>.<br/>
-    <a href="https://www.linkedin.com/in/ongjeffrey/">LinkedIn</a>.</p>
+      <div class="center">
+        <h1 class="header blink">Jeff Ong</h1>
+      </div>
 
-    <p><em>Thank you for visiting.</em></p>
+      <div class="block tl">
+        <div class="padded">
+          <h2 class="header">About</h2>
+          <p>I'm an American artist | designer | technologist based in Brooklyn, New York.</p>
+          <p>I graduated from the University of Michigan and the <a href="https://tisch.nyu.edu/itp">Interactive Telecommunications Program</a> at New York University.</p>
+          <p>I work at <a href="https://frogdesign.com">frog design</a>, advancing the human experience through design and technology.</p>
+        </div>
+      </div>
+
+      <div class="block tr">
+        <div class="padded right">
+          <h2 class="header">Work</h2>
+          <p>
+            <?php foreach($projects as $project): ?>
+            <a href="<?= $project->url() ?>"><?= $project->title() ?></a><br>
+            <?php endforeach ?>
+          </p>
+        </div>
+      </div>
+
+      <div class="block bl">
+        <div class="padded">
+          <h2 class="header">Contact + Info</h2>
+          <p>
+            <a href="mailto:this.jeffong@gmail.com">Email</a><br>
+            <a href="https://github.com/jffng">GitHub</a><br>
+            <a href="https://www.linkedin.com/in/ongjeffrey/">LinkedIn</a>
+          </p>
+        </div>
+        </p>
+      </div>
+
+      <div class="block br">
+        <div class="padded right">
+          <h2 class="header">Societal Validation</h2>
+          <p>
+            <a href="mailto:this.jeffong@gmail.com">Atlantic</a><br>
+            <a href="https://github.com/jffng">New York Times</a><br>
+            <a href="https://github.com/jffng">Resistor</a><br>
+            <a href="https://github.com/jffng">Shows</a><br>
+          </p>
+        </div>
+      </div>
+    </div>
 </body>
 </html>
