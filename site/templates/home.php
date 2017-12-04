fdd8bd9e101d8fa1e92c1d5112fecfadac4cf792
Jeff Ong <jonger4@gmail.com>
2017-12-04 08:49:08 -0500

    removing work

--- site/templates/home.php
+++ site/templates/home.php
@@ -6,11 +6,11 @@ $projects = page('projects')->children()->visible();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Jeff Ong | Portfolio of design technology and art</title>
+    <title>Jeff Ong | design . technology . art</title>
     <link href="https://fonts.googleapis.com/css?family=Rasa|Roboto" rel="stylesheet">
     <link href="/assets/css/index.css" rel="stylesheet">
     <meta charset="UTF-8">
-    <meta name="description" content="Portfolio media for Jeff Ong">
+    <meta name="description" content="The online representation & portfolio of Jeff Ong. Design + technology + art on sale! Projects going quickly! Hire today!">
     <meta name="keywords" content="design,technology,code,media,itp,frog,art,jeff ong,jeffrey ong">
     <meta name="author" content="Jeff Ong">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,13 +28,13 @@ $projects = page('projects')->children()->visible();
           <h2 class="header">About</h2>
           <p>I'm an American artist | designer | technologist based in Brooklyn, New York.</p>
           <p>I graduated from the University of Michigan and the <a target="_blank" href="https://tisch.nyu.edu/itp">Interactive Telecommunications Program</a> at New York University.</p>
-          <p>I work at <a target="_blank" href="https://frogdesign.com">frog design</a>, advancing the human experience through design and technology.</p>
+          <p>I work at <a target="_blank" href="https://www.frogdesign.com">frog design</a>, advancing the human experience through design and technology.</p>
         </div>
       </div>
 
       <div class="block tr">
         <div class="padded right">
-          <h2 class="header">Work</h2>
+          <!-- <h2 class="header">Work</h2> -->
           <p>
             <?php foreach($projects as $project):?>
             <?php
@@ -46,7 +46,7 @@ $projects = page('projects')->children()->visible();
               }
             endforeach
             ?>
-            <a target="_blank" class="project-link" data-thumbnail="<?= $thumbnail ?>" href="<?= $project->url() ?>"><?= $project->title() ?></a><br>
+            <!-- <a target="_blank" class="project-link" data-thumbnail="<?= $thumbnail ?>" href="<?= $project->url() ?>"><?= $project->title() ?></a><br> -->
             <?php endforeach ?>
           </p>
         </div>
@@ -54,20 +54,7 @@ $projects = page('projects')->children()->visible();
 
       <div class="block bl">
         <div class="padded">
-          <h2 class="header">Contact + Info</h2>
-          <p>
-            <a target="_blank" href="mailto:this.jeffong@gmail.com">Email</a><br>
-            <a target="_blank" href="https://github.com/jffng">GitHub</a><br>
-            <a target="_blank" href="https://www.linkedin.com/in/ongjeffrey/">LinkedIn</a><br>
-            <a target="_blank" href="http://jffng.com/content/3-about/0-jeffong/jeff-ong-resume_2017.pdf">CV</a>
-          </p>
-        </div>
-        </p>
-      </div>
-
-      <div class="block br">
-        <div class="padded right">
-          <h2 class="header">Societal Validation</h2>
+          <!-- <h2 class="header">Societal Validation</h2>
           <p>
             <a target="_blank" href="http://eng.indiebo.co/indiebox/">Indie Bogota - 2017</a><br>
             <a target="_blank" href="http://www.nytransitmuseum.org/platform/">Platform: NY Transit Museum Exhibit - 2016</a><br>
@@ -77,6 +64,19 @@ $projects = page('projects')->children()->visible();
             <a target="_blank" href="https://muse.jhu.edu/article/552317">Leonardo Volume #47 - 2014</a><br>
             <a target="_blank" href="http://s2014.siggraph.org/attendees/art-gallery/events/subway-stories.html">SIGGRAPH Art Gallery - 2014</a><br>
             <a target="_blank" href="https://creators.vice.com/en_us/article/pgqdgz/subway-stories-takes-you-on-an-interactive-ride-through-the-nyc-underground-sans-stressawfulness">The Creators Project - 2014</a><br>
+          </p> -->
+        </div>
+        </p>
+      </div>
+
+      <div class="block br">
+        <div class="padded right">
+          <h2 class="header">Contact + Info</h2>
+          <p>
+            <a target="_blank" href="mailto:this.jeffong@gmail.com">Email</a><br>
+            <a target="_blank" href="https://github.com/jffng">GitHub</a><br>
+            <a target="_blank" href="https://www.linkedin.com/in/ongjeffrey/">LinkedIn</a><br>
+            <a target="_blank" href="http://jffng.com/content/3-about/0-jeffong/jeff-ong-resume_2017.pdf">CV</a>
           </p>
         </div>
       </div>
